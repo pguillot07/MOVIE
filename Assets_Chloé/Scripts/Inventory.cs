cc59691264545fa35da71ecb691af98eb84d85e7
Chloé Vogel <chloe.vogel@mines-paristech.fr>
2021-10-25 09:47:26 +0200

    Mise à jour de l'inventaire

--- Assets_Chloé/Scripts/Inventory.cs
+++ Assets_Chloé/Scripts/Inventory.cs
@@ -6,35 +6,44 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    public bool inventoryEnabled;
+    public bool inventoryEnabled = false;
+    public GameObject panel;
     public GameObject inventory;
-    public GameObject slotHolder;
-
-    //Sélectionne l'objet qui va être placé dans l'inventaire
-    public GameObject object1;
 
     private int allSlots;
     private int enabledSlots;
     private GameObject[] slot;
 
+    //Sélectionne l'objet qui va être placé dans l'inventaire
+    public GameObject[] listInventory;
+    private int allItemsInventory;
 
+    private bool makeUpdate = false;
+    
     void Start()
-    { 
-
+    {   //Commence par lire toutes les cases de l'inventaire et marque vide celles qui le sont
+        
         allSlots = 27; //A modifier si on modifie le nombre de cases de l'inventaire
         slot = new GameObject[allSlots];
-
-        //Commence par lire toutes les cases de l'inventaire et marque vide celles qui le sont
+        
         for (int i=0; i < allSlots; i++){
-            slot[i] = slotHolder.transform.GetChild(i).gameObject;
-
-            if (slot[i].GetComponent<Slot>().item == null)
+            slot[i] = inventory.transform.GetChild(i).gameObject;
+            slot[i].GetComponent<Slot>().IDslot = i;
+            if (slot[i].GetComponent<Slot>().item == null){
                 slot[i].GetComponent<Slot>().empty = true;
+            }
+        }
+        GameObject object1;
+        Items item;
+        allItemsInventory = listInventory.Length;
+        for (int i=0; i < allItemsInventory; i++){
+            object1 = listInventory[i];
+            item = object1.GetComponent<Items>();
+            Debug.Log(item.ID);
+            AddToInventory(object1, item.ID, item.description, item.icon);
         }
 
-        //Fait disparaître object1 de la map pour le placer dans l'inventaire 
-        Items item = object1.GetComponent<Items>();
-        AddToInventory(object1, item.ID, item.description, item.icon);
+        
     }
 
     void AddToInventory(GameObject itemObject, int itemID, string itemDescription, Sprite itemIcon){
@@ -46,30 +55,51 @@ public class Inventory : MonoBehaviour
                 slot[i].GetComponent<Slot>().ID = itemID;
                 slot[i].GetComponent<Slot>().description = itemDescription;
 
-                itemObject.transform.parent = slot[i].transform;
+                //itemObject.transform.parent = slot[i].transform;
                 itemObject.SetActive(false); //On fait disparaître l'objet de la map
 
-                slot[i].GetComponent<Slot>().UpdateSlot(); //On fait apparaître l'icone de l'objet dans la cellule
+                
                 slot[i].GetComponent<Slot>().empty = false; //La cellule n'est plus vide
+
+                return;
             }
-            return;
         }
+
+    }
+    
+    public void OutOfInventory(int IDslot){
+        GameObject itemObject = slot[IDslot].GetComponent<Slot>().item;
+
+        Vector3 playerPosition = GetComponent<Transform>().position;
+        Vector3 itemPosition = playerPosition + new Vector3(1f,0f,0f);
+    
+        GameObject clone = Instantiate(itemObject);
+        clone.SetActive(true);
+        Debug.Log("OutOfInventory");
     }
 
 
     void Update()
     {
         //Affichage de l'inventaire avec la touche I
-        if (Input.GetKeyDown(KeyCode.I))
+        if (Input.GetKeyDown(KeyCode.I)){
             inventoryEnabled = !inventoryEnabled;
-        
-        if (inventoryEnabled == true)
-        {
-            inventory.SetActive(true);
+            if (inventoryEnabled == true)
+            {   
+                panel.SetActive(true);
+            }
+            else{
+                panel.SetActive(false);
+            }
+            makeUpdate = true;
         }
-        else{
-            inventory.SetActive(false);
+    
+
+        if (makeUpdate){
+            for (int i = 0; i < allSlots; i++){
+                    slot[i].GetComponent<Slot>().UpdateSlot(); //On fait apparaître l'icone de l'objet dans la cellule
+                }
+            makeUpdate = false;
         }
     }
-
 }
--- Assets_Chloé/Scripts/Slot.cs
+++ Assets_Chloé/Scripts/Slot.cs
@@ -2,21 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 //Component commun à tous les Slots de l'inventaire
 
-public class Slot : MonoBehaviour
+public class Slot : MonoBehaviour, IPointerClickHandler //, IDragHandler, IEndDragHandler
 {
     //Possède les mêmes caractéristiques que Items
     public GameObject item;
     public int ID;
     public string description;
-    //Deux caract. supplémentaires
+    //Quatre caract. supplémentaires
     public bool empty;
     public Sprite icon;
+    public int IDslot;
+    public GameObject Player;
+
+    private Vector3 initialPosition; 
 
     //Permet d'afficher l'icone de l'objet dans la cellule
     public void UpdateSlot(){
         this.GetComponent<Image>().sprite = icon;
+        initialPosition = transform.position;
+        Debug.Log(initialPosition);
     }
+
+    public void OnPointerClick(PointerEventData pointerEventData){
+        if (!empty){
+            Debug.Log("pop!");
+            Inventory inventory = Player.GetComponent<Inventory>();
+            inventory.OutOfInventory(IDslot);
+        }
+        
+    }
+/* 
+    public void OnDrag(PointerEventData eventData){
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData){
+        transform.position = initialPosition;
+    } */
 }
